5735d94aa712379da6f4e78eab43227fdc218f3c
xuan 黄 <xuanhuang@sinx.local>
2025-10-10 15:20:07 +0800

    recover bugs.

--- docs/.vitepress/views/BlogArchive.vue
+++ docs/.vitepress/views/BlogArchive.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script setup lang="ts">
-import { useData } from "vitepress";
+import { useData, useRoute } from "vitepress";
 import { data as posts } from "../utils/article.data.js";
 import PostCard from "./BlogArchivePostCard.vue";
 import Sidebar from "./BlogArchiveSidebar.vue";
@@ -54,7 +54,8 @@ const { frontmatter: pageData, theme } = useData();
 const { hero, types, features, flow } = pageData.value;
 
 // 根据当前 page 名称获取 sidebar 数据并构造相应的类别
-const pathname = window.location.pathname;
+const route = useRoute();
+const pathname = route.path;
 const sidebarData = theme.value.sidebar?.[pathname];
 const categories =
   types || sidebarData?.items.map((item: any) => ({ name: item.text, link: item.link }));
